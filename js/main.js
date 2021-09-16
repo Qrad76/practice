c34c9f0260861f29abec30ce561ad17e6b3d4473
Qrad76 <qrad76@gmail.com>
2021-09-16 22:54:50 +0900

    Js 데이터 - 문자 연습

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// const str = 'Hello world!'
+
+
+// String.prototype.indexOf() 문자의 순서
+// String.prototype.slice()  문자 자르기
+// String.prototype.replace() 문자 교체, 제거
+//const str = 'Qrad76@gmail.com'
+//console.log(str.match(/.+(?=@)/)[0]) 정규표현식 이용
+//console.log(str.trim()) 띄어쓰기 제거
+
+
+const pi = 3.14159265358979;
+console.log(pi);
+
+const str = pi.toFixed(2);
+console.log(str)
+console.log(typeof str)
+
+const integer = parseInt(str);
+const float = parseFloat(str);
+
+console.log(integer, float)
+
