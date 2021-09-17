7510e14a858bd4a5f3a7d57b225c077576783ead
Qrad76 <qrad76@gmail.com>
2021-09-17 13:42:00 +0900

    2021/09/17 Study_1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-// const str = 'Hello world!'
-
-
+//문자
 // String.prototype.indexOf() 문자의 순서
 // String.prototype.slice()  문자 자르기
 // String.prototype.replace() 문자 교체, 제거
-//const str = 'Qrad76@gmail.com'
 //console.log(str.match(/.+(?=@)/)[0]) 정규표현식 이용
 //console.log(str.trim()) 띄어쓰기 제거
 
+//숫자
+//const str = pi.toFixed(2); 소숫점 2째짜리 까지만 표현 (String 형으로 자동 변경됨)
+// const integer = parseInt(str); int 형으로 형변환
+// const float = parseFloat(str); float 형으로 형변환
 
-const pi = 3.14159265358979;
-console.log(pi);
+//Math
+// console.log('abs', Math.abs(-12));  //절대값 표현 12
+// console.log('min', Math.min(2,8)); // 최소값 반환 2
+// console.log('max', Math.Max(2,8)); // 최대값 반환 8
+// console.log('ceil', Math.ceil(3.14)); // 올림 4
+// console.log('floor', Math.floor(3.14)); // 내림 3
+// console.log('round', Math.round(3.5)); // 반올림 4
+// console.log('random', Math.random()); // 0.XXXX 형태로 난수 발생
 
-const str = pi.toFixed(2);
-console.log(str)
-console.log(typeof str)
+const numbers = [1, 2, 3, 4]
+const fruits = ['Apple', 'Banana', 'Cherry']
 
-const integer = parseInt(str);
-const float = parseFloat(str);
-
-console.log(integer, float)
-
+console.log(numbers[1])
+console.log(fruits[2])
